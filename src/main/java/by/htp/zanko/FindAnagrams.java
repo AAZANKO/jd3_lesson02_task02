0bf2571ae3b711315412add3672ced754206a6cc
AAZANKO <aleksandr.zan87@gmail.com>
2019-03-01 17:32:53 +0300

    one to commit

---
+++ src/main/java/by/htp/zanko/FindAnagrams.java
@@ -0,0 +1,34 @@
+package by.htp.zanko;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindAnagrams {
+
+    public Map<String, String> getFindAnagrams(String[] arrayString) {
+
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < arrayString.length; i++) {
+
+            String tmpWord = arrayString[i];
+
+            char[] sym = arrayString[i].toCharArray();
+
+            Arrays.sort(sym);
+
+            String wordKey = new String(sym);
+
+            String value = map.get(wordKey);
+
+            if (value == null) {
+                map.put(wordKey, tmpWord);
+            } else {
+                map.put(wordKey, value + " " + tmpWord);
+            }
+        }
+        return map;
+    }
+
+}
---
+++ src/test/java/by/htp/zanko/FindAnagramsTest.java
@@ -0,0 +1,32 @@
+package by.htp.zanko;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class FindAnagramsTest {
+
+    @Test
+    public void checkGetFindAnagrams(){
+
+        String[] arrayString = new String[10];
+
+        FindAnagrams findAnagrams = new FindAnagrams();
+
+        arrayString[0] = "asd";
+        arrayString[1] = "dsa";
+        arrayString[2] = "qwe";
+        arrayString[3] = "wer";
+        arrayString[4] = "rty";
+        arrayString[5] = "gfh";
+        arrayString[6] = "vbn";
+        arrayString[7] = "acxv";
+        arrayString[8] = "vbcv";
+        arrayString[9] = "nbv";
+
+        Map<String, String> map = findAnagrams.getFindAnagrams(arrayString);
+        Assert.assertNotNull(map);
+    }
+
+}
